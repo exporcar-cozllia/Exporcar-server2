f0859807c13ee03580f489a8faf4430aba358414
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-04 17:56:54 +0900

    커스텀 유저 시비스

---
+++ kjone-user-service/src/main/java/com/kjone/kjoneuserservice/security/service/CustomUserDetailService.java
@@ -0,0 +1,9 @@
+package com.kjone.kjoneuserservice.security.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface CustomUserDetailService extends UserDetailsService {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException;
+}
