894d465c9ba3d0fd887f7084cf11b89d4f10fd4a
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-04 17:59:00 +0900

    수정

--- kjone-user-service/src/main/java/com/kjone/kjoneuserservice/security/SecurityConfig.java
+++ kjone-user-service/src/main/java/com/kjone/kjoneuserservice/security/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.kjone.kjoneuserservice.security;
 
+import com.kjone.kjoneuserservice.security.cookie.CookieProvider;
+import com.kjone.kjoneuserservice.security.jwt.JwtAuthenticationFilter;
+import com.kjone.kjoneuserservice.security.jwt.JwtProvider;
+import com.kjone.kjoneuserservice.security.service.CustomUserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
