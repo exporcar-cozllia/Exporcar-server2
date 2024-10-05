fc77917ba68d001cbef761c8df4306c418080d33
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-06 02:49:45 +0900

    커스텀 유저 디테일서비스레이어

--- kjone-organization-service/src/main/java/com/kjone/kjoneorganizationservice/security/service/impl/CustomUserDetailServiceImpl.java
+++ kjone-organization-service/src/main/java/com/kjone/kjoneorganizationservice/security/service/impl/CustomUserDetailServiceImpl.java
@@ -1,4 +1,27 @@
 package com.kjone.kjoneorganizationservice.security.service.impl;
 
-public class CustomUserDetailServiceImpl {
+import com.kjone.kjoneorganizationservice.domain.organization_user.OrgForSecurity;
+import com.kjone.kjoneorganizationservice.domain.organization_user.Organization_User;
+import com.kjone.kjoneorganizationservice.repository.OrganizationUserRepository;
+import com.kjone.kjoneorganizationservice.security.service.CustomUserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailServiceImpl implements CustomUserDetailService {
+
+    private final OrganizationUserRepository organizationUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Organization_User organization_user = organizationUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + email));
+        return OrgForSecurity.builder()
+                .organization_user(organization_user)
+                .build();
+    }
+
 }
