408c7a81eb4222ef509166986afaa6fa67ee29cc
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-06 00:48:54 +0900

    유래카 서버 애플리케이션 테스트

---
+++ kjone-eureka-server/src/test/java/com/kjone/kjoneeurekaserver/KjoneEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjone.kjoneeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjoneEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
