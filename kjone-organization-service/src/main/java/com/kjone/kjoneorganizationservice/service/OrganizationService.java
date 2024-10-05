0fe5c7e54fffc6d37cfb0df6dbd87f934154b393
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-06 03:01:05 +0900

    조직 서비스

--- kjone-organization-service/src/main/java/com/kjone/kjoneorganizationservice/service/OrganizationService.java
+++ kjone-organization-service/src/main/java/com/kjone/kjoneorganizationservice/service/OrganizationService.java
@@ -1,4 +1,10 @@
 package com.kjone.kjoneorganizationservice.service;
 
+import com.kjone.kjoneorganizationservice.domain.organization.Organization;
+
 public interface OrganizationService {
+    // organization 생성/ 관리
+
+    public void createOrganization(Organization organization, String sender) throws Exception;
+    public Organization getOrganizationName(String name, String sender) throws Exception;
 }
