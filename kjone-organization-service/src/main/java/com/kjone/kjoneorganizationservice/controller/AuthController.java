ffc1993b487929661b33394cab2dcb142b8bf9f2
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-06 02:09:29 +0900

    auth 컨트롤러

---
+++ kjone-organization-service/src/main/java/com/kjone/kjoneorganizationservice/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.kjone.kjoneorganizationservice.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/organization/sign")
+@RequiredArgsConstructor
+public class AuthController {
+
+}
