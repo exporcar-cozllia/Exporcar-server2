334659576c9390fd0019b7b108f7c609ecc9a86c
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-06 18:55:26 +0900

    조직 생성 엔드포인트 인증 여부 추가

--- kjone-organization-service/src/main/java/com/kjone/kjoneorganizationservice/controller/OrganizationController.java
+++ kjone-organization-service/src/main/java/com/kjone/kjoneorganizationservice/controller/OrganizationController.java
@@ -1,15 +1,19 @@
 package com.kjone.kjoneorganizationservice.controller;
 
 import com.kjone.kjoneorganizationservice.domain.organization.Organization;
+import com.kjone.kjoneorganizationservice.expection.GlobalException;
 import com.kjone.kjoneorganizationservice.service.OrganizationService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/v1/organization")
 @RequiredArgsConstructor
 public class OrganizationController {
 
@@ -17,14 +21,35 @@ public class OrganizationController {
 
 
     // 조직 생성 엔드포인트
+//    @PostMapping("/create")
+//    public ResponseEntity<String> createOrganization(@RequestBody Organization organization, Authentication authentication) {
+//        // 인증된 사용자 확인
+//        if (authentication != null) {
+//            System.out.println("인증된 사용자: " + authentication.getName());
+//            try {
+//                // 조직 생성 서비스 호출
+//                organizationService.createOrganization(organization, authentication);
+//                return ResponseEntity.status(HttpStatus.CREATED).body("조직이 성공적으로 생성되었습니다.");
+//            } catch (GlobalException e) {
+//                // 사용자 정의 예외 처리
+//                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+//            } catch (Exception e) {
+//                // 일반 예외 처리
+//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("조직 생성에 실패했습니다.");
+//            }
+//        }
+//        return new ResponseEntity<>("인증되지 않은 사용자입니다.", HttpStatus.UNAUTHORIZED);
+//    }
     @PostMapping("/create")
-    public ResponseEntity<String> createOrganization(@RequestBody Organization organization, @RequestParam String sender) {
-        try {
+    public ResponseEntity<?> createOrganization(@RequestBody Organization organization, HttpServletRequest request, @RequestParam String sender) throws Exception {
+        // 인증된 사용자만 로그아웃을 허용
+        if (SecurityContextHolder.getContext().getAuthentication() != null) {
+//            System.out.println("Create User: " + SecurityContextHolder.getContext().getAuthentication().getName());
+
             organizationService.createOrganization(organization, sender);
-            return ResponseEntity.status(HttpStatus.CREATED).body("조직이 성공적으로 생성되었습니다.");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.OK);
         }
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN); // 인증되지 않은 사용자 접근 시 403 반환
     }
 
 
