d13d37750c804d7f89a7dd55a815ce5703a12c87
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-06 18:55:54 +0900

    조직 서비스 이메일 인증 추가

--- kjone-organization-service/src/main/java/com/kjone/kjoneorganizationservice/service/OrganizationService.java
+++ kjone-organization-service/src/main/java/com/kjone/kjoneorganizationservice/service/OrganizationService.java
@@ -1,10 +1,14 @@
 package com.kjone.kjoneorganizationservice.service;
 
 import com.kjone.kjoneorganizationservice.domain.organization.Organization;
+import org.springframework.security.core.Authentication;
 
 public interface OrganizationService {
     // organization 생성/ 관리
 
     public void createOrganization(Organization organization, String sender) throws Exception;
+
+//    public void createOrganization(Organization organization, Authentication authentication) throws Exception;
+
     public Organization getOrganizationName(String name, String sender) throws Exception;
 }
