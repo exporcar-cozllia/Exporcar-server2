e9c4fe4081ab313cc2f882465cfcc2faa4ec1fc9
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-07 09:32:32 +0900

    WorkSpaceService 애플리케이션 테스트

---
+++ kjone-workspace-service/src/test/java/com/kjone/kjoneworkspaceservice/KjoneWorkspaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjone.kjoneworkspaceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjoneWorkspaceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
