57fecd3f9fd5f192cb1e4c5ff5a0e8dde253813a
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-07 09:32:23 +0900

    WorkSpaceService 애플리케이션

---
+++ kjone-workspace-service/src/main/java/com/kjone/kjoneworkspaceservice/KjoneWorkspaceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kjone.kjoneworkspaceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjoneWorkspaceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KjoneWorkspaceServiceApplication.class, args);
+    }
+
+}
