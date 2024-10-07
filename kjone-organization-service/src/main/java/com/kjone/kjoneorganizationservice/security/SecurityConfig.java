9baa3fe2a1a9301771ad18f5a939baa5ef9872bd
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-07 09:50:19 +0900

    조직 조회 엔드프인트 권한 설정

--- kjone-organization-service/src/main/java/com/kjone/kjoneorganizationservice/security/SecurityConfig.java
+++ kjone-organization-service/src/main/java/com/kjone/kjoneorganizationservice/security/SecurityConfig.java
@@ -41,11 +41,11 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(HttpMethod.POST, "/v1/organization/sign/signup", "/v1/organization/sign/signin").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/v1/organization/user/me").authenticated() // 인증된 유저만
                         .requestMatchers(HttpMethod.POST, "/v1/organization/create").hasRole("OWNER") //권한이 OWNER인 유저만
+                        .requestMatchers(HttpMethod.POST, "/v1/organization/{name}").hasAnyRole("OWNER", "ADMIN", "EMPLOYEE") //조직 조회 권한
                         .requestMatchers(HttpMethod.GET, "/v1/organization/sign/signout").authenticated() // 인증된 유저만
-                        .requestMatchers(HttpMethod.DELETE, "/v1/organization/sign/delete").permitAll() // 인증된 유저만
+                        .requestMatchers(HttpMethod.DELETE, "/v1/organization/sign/delete").authenticated() // 인증된 유저만
                         .requestMatchers("/favicon.ico").permitAll() // Favicon에 대한 접근 허용
                         .anyRequest().denyAll());
 
