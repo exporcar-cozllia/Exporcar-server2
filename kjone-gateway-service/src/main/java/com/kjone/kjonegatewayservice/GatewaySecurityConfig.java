0b4899eb51872b244ac0cabf1569adbea3364d9d
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-08 15:24:36 +0900

    게이트웨이 시큐리티 설정 일단 보류

--- kjone-gateway-service/src/main/java/com/kjone/kjonegatewayservice/GatewaySecurityConfig.java
+++ kjone-gateway-service/src/main/java/com/kjone/kjonegatewayservice/GatewaySecurityConfig.java
@@ -6,27 +6,50 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+import java.util.Arrays;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class GatewaySecurityConfig {
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .cors(AbstractHttpConfigurer::disable)
+//                .httpBasic(AbstractHttpConfigurer::disable)
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .requestMatchers(HttpMethod.POST, "/v1/sign/signup", "/v1/sign/signin").permitAll()
+//                        .requestMatchers("/api/**").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/auth/**").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/v1/user/me").hasAnyRole("USER", "ADMIN", "OWNER", "EMPLOYEE")
+//                        .requestMatchers(HttpMethod.GET, "/v1/sign/signout").authenticated() // 인증된 유저만
+//                        .requestMatchers(HttpMethod.DELETE, "/v1/sign/delete").authenticated() // 인증된 유저만
+//                        .requestMatchers("/favicon.ico").permitAll() // Favicon에 대한 접근 허용
+//                        .anyRequest().denyAll());
+//        return http.build();
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers(HttpMethod.POST, "/v1/sign/signup", "/v1/sign/signin").permitAll()  // 인증 필요 없음
-                        .requestMatchers("/api/**").permitAll()  // 모든 API 접근 허용
-                        .requestMatchers(HttpMethod.GET, "/v1/user/me").permitAll()  // 특정 경로에 대해 인증 요구 안함
-                        .requestMatchers(HttpMethod.GET, "/v1/sign/signout").authenticated()  // 이 경로는 인증된 사용자만
-                        .anyRequest().denyAll()
-                );
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(HttpMethod.GET, "/test/test").permitAll() // 요청 허용
+                                .anyRequest().authenticated() // 그 외의 요청은 인증 필요
+                )
+                .csrf().disable(); // CSRF 비활성화
+
         return http.build();
     }
 }
