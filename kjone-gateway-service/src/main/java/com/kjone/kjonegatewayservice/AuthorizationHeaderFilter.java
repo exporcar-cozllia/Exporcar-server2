4a0dc9bc69a50e82da23cb160de6dc78c1beedf1
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-08 13:39:36 +0900

    AuthorizationHeaderFilter 설정

--- kjone-gateway-service/src/main/java/com/kjone/kjonegatewayservice/AuthorizationHeaderFilter.java
+++ kjone-gateway-service/src/main/java/com/kjone/kjonegatewayservice/AuthorizationHeaderFilter.java
@@ -1,4 +1,25 @@
 package com.kjone.kjonegatewayservice;
 
-public class AuthorizationHeaderFilter {
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    public AuthorizationHeaderFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            // 필터 로직을 여기에 추가
+            return chain.filter(exchange);
+        };
+    }
+
+    public static class Config {
+        // 필터 구성 속성
+    }
 }
