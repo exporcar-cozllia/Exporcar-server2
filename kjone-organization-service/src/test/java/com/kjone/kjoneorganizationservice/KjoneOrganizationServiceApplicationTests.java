9135eae6cfdbd5feee33ffd2c7d9320a89d9d8da
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-11 17:10:23 +0900

    애플리케이션 테스트 수행

--- kjone-organization-service/src/test/java/com/kjone/kjoneorganizationservice/KjoneOrganizationServiceApplicationTests.java
+++ kjone-organization-service/src/test/java/com/kjone/kjoneorganizationservice/KjoneOrganizationServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KjoneOrganizationServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
