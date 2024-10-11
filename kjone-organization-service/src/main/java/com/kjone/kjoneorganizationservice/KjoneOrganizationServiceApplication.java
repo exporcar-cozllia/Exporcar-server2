7faadb4619940c8e087280224327634c71424edf
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-11 17:10:17 +0900

    애플리케이션 수행

--- kjone-organization-service/src/main/java/com/kjone/kjoneorganizationservice/KjoneOrganizationServiceApplication.java
+++ kjone-organization-service/src/main/java/com/kjone/kjoneorganizationservice/KjoneOrganizationServiceApplication.java
@@ -2,14 +2,12 @@ package com.kjone.kjoneorganizationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class KjoneOrganizationServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KjoneOrganizationServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(KjoneOrganizationServiceApplication.class, args);
+	}
 
 }
