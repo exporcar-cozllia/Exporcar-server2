e2c4c3654d6a130b230c6a1ba8548a113a92c6df
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-12 14:15:48 +0900

    Application

---
+++ exporcar-organization-service/src/main/java/com/kjone/exporcarorganizationservice/ExporcarOrganizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kjone.exporcarorganizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExporcarOrganizationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExporcarOrganizationServiceApplication.class, args);
+    }
+
+}
